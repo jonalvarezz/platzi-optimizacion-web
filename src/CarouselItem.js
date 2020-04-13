dd58df8674db050d2382ad1a4cf6120d9872d74f
Jonathan Alvarez <j@jonalvarezz.com>
2020-04-13 16:33:49 +0200

    Reduce bundle size by 70%

--- src/CarouselItem.js
+++ src/CarouselItem.js
@@ -1,7 +1,9 @@
 import h from 'hyperscript'
-import moment from 'moment'
+import formatDistance from 'date-fns/formatDistance'
+import parseISO from 'date-fns/parseISO'
 
-const relativeDate = dateStr => moment(dateStr, 'YYYY-MM-DD').fromNow()
+const relativeDate = dateStr =>
+  formatDistance(parseISO(dateStr, 'YYYY-MM-DD'), new Date())
 
 const Controls = ({ slug, youtubeVideoId }) =>
   h(
