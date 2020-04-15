971084d214775f10ce833e37e767b6dd2f1f305e
Jonathan Alvarez <j@jonalvarezz.com>
2020-04-15 17:58:35 +0200

    Focus only in the home page.
    And adjusts the colophon

--- src/index.js
+++ src/index.js
@@ -1,8 +1,61 @@
-import { withRouteProtection, handleLogin, handleLogout } from './login'
+import h from 'hyperscript'
+import { fetchPopular, fetchHighestRated, fetchTrending } from './api'
+import CarouselItem from './CarouselItem'
 
-withRouteProtection()
+const SectionTitle = title => h('h3.carousel-title', title)
 
-window.addEventListener('DOMContentLoaded', () => {
-  handleLogin()
-  handleLogout()
+const Carousel = ({ itemsList = [] }) =>
+  h(
+    'section.carousel',
+    h(
+      'div',
+      itemsList.map(
+        ({
+          attributes: { titles, posterImage, slug, youtubeVideoId, startDate },
+        }) =>
+          CarouselItem({
+            imageUrl: posterImage.large,
+            title: titles.en,
+            subtitle: titles.ja_jp,
+            slug,
+            youtubeVideoId,
+            startDate,
+          })
+      )
+    )
+  )
+
+window.addEventListener('DOMContentLoaded', async () => {
+  const mountReference = document.querySelector('.main').lastElementChild
+
+  if (!mountReference) {
+    return 0
+  }
+
+  const trending = await fetchTrending()
+  const popular = await fetchPopular()
+  const highestRated = await fetchHighestRated()
+
+  mountReference
+    .insertAdjacentElement('afterend', SectionTitle('Trending Anime'))
+    .insertAdjacentElement(
+      'afterend',
+      Carousel({
+        itemsList: trending,
+      })
+    )
+    .insertAdjacentElement('afterend', SectionTitle('Highest Rated Anime'))
+    .insertAdjacentElement(
+      'afterend',
+      Carousel({
+        itemsList: highestRated,
+      })
+    )
+    .insertAdjacentElement('afterend', SectionTitle('Most Popular Anime'))
+    .insertAdjacentElement(
+      'afterend',
+      Carousel({
+        itemsList: popular,
+      })
+    )
 })
